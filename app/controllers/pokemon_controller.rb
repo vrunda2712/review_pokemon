aa56d97bb600e22cb2b7b0eff47b886c3637a0ca
vrunda2712 <angy.v27@gmail.com>
2017-07-14 11:22:54 -0400

    Tried diffrent method to display API data with JSON

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,6 +1,19 @@
 class PokemonController < ApplicationController
 
   def index
-    
+    max_per_page = 15
+    offset = 0
+    @pokemon = []
+
+    while offset < 100 do
+      pokemon_response = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/?limit=#{max_per_page}&offset=#{offset}")
+      # @pokemon = pokemon_response.body
+      pokemon_json_data = pokemon_response.body
+      @pokemon_data = JSON.parse(pokemon_json_data)
+      @pokemon.concat(@pokemon_data["results"])
+
+      offset += 15
+    end
+
   end
 end
